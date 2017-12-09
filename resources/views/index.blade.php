a29e9f2d700ad3246bb93fa57ec29e5a61dbd019
Neobrinoke <alexis19@hotmail.fr>
2017-12-09 21:07:13 +0100

    Remove guest part on index layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,29 +11,22 @@
 	<body>
 		<header class="ui attached stackable menu inverted" style="margin-bottom: 25px;">
 			<div class="ui container">
-				@guest
-					<div class="right menu">
-						<a href="{{ route('register') }}" class="ui item">Inscription</a>
-						<a href="{{ route('login') }}" class="ui item">Connexion</a>
-					</div>
-				@else
-					<a href="{{ route('todo.browse') }}" class="ui item"><i class="list icon"></i>Afficher mes listes</a>
-					<div class="right menu">
-						<div class="ui dropdown icon item">
-							<span><i class="user circle outline icon"></i>{{ Auth::user()->name }}<i class="dropdown icon"></i></span>
-							<div class="menu">
-								<p class="item">Connecté en tant que <strong>{{ Auth::user()->name }}</strong></p>
-								<div class="divider"></div>
-								<a href="/user/account/" class="item"><i class="settings icon"></i>Mon compte</a>
-								<div class="divider"></div>
-								<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="item"><i class="sign out icon"></i>Déconnexion</a>
-							</div>
+				<a href="{{ route('todo.browse') }}" class="ui item"><i class="list icon"></i>Afficher mes listes</a>
+				<div class="right menu">
+					<div class="ui dropdown icon item">
+						<span><i class="user circle outline icon"></i>{{ Auth::user()->name }}<i class="dropdown icon"></i></span>
+						<div class="menu">
+							<p class="item">Connecté en tant que <strong>{{ Auth::user()->name }}</strong></p>
+							<div class="divider"></div>
+							<a href="/user/account/" class="item"><i class="settings icon"></i>Mon compte</a>
+							<div class="divider"></div>
+							<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="item"><i class="sign out icon"></i>Déconnexion</a>
 						</div>
 					</div>
-					<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-						{{ csrf_field() }}
-					</form>
-				@endguest
+				</div>
+				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+					{{ csrf_field() }}
+				</form>
 			</div>
 		</header>
 		<main class="ui container">
