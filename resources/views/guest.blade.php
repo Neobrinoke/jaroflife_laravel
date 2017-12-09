d87d7d9b69351c2057d2c7f43e20202560c775b6
Neobrinoke <alexis19@hotmail.fr>
2017-12-09 20:21:39 +0100

    Guest layout

---
+++ resources/views/guest.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>@yield('title')</title>
+		<link rel="stylesheet" href="{{ asset('css/semantic.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/guest.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	</head>
+	<body>
+		<div class="ui middle aligned center aligned grid">
+			<div class="column">
+				<h2 class="ui teal image header">
+					<div class="content">@yield('title')</div>
+				</h2>
+				@yield('container')
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="{{ asset('js/tablesort.js') }}"></script>
+		<script src="{{ asset('js/semantic.js') }}"></script>
+		<script src="{{ asset('js/script.js') }}"></script>
+	</body>
+</html>
