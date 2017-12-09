4ef2dfb92969c3f06d5234faff4de42f1dcd90ad
Neobrinoke <alexis19@hotmail.fr>
2017-12-09 20:16:34 +0100

    Middleware

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,6 +10,11 @@ use App\Task;
 
 class TaskController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+	
 	/**
 	 * Display a listing of the resource.
 	 *
